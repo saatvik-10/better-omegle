c70627bef8782d6257483552f3a0426ba94cc7f2
saatvik-10 <saatvikmadan529@gmail.com>
2026-05-16 00:58:03 +0530

    added router dom parts

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,19 @@
-import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
+import Landing from './screens/Landing';
+import Room from './screens/Room';
 
 function App() {
   return (
     <>
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Landing />} />
+          <Route path='/room' element={<Room />} />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
