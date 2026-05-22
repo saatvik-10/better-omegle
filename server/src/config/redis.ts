8aa71207c893a0cc3ca4a6025663eabd98524caf
saatvik-10 <saatvikmadan529@gmail.com>
2026-05-22 12:25:47 +0530

    updated redis environmrnt

--- server/src/config/redis.ts
+++ server/src/config/redis.ts
@@ -1,6 +1,7 @@
 import { createClient } from 'redis';
 
-const client = createClient();
+const redisUrl = process.env.REDIS_URL ?? 'redis://localhost:6379';
+const client = createClient({ url: redisUrl });
 
 client.on('error', (err) => console.log('Redis client error', err));
 
